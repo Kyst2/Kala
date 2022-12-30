05b945486ddfd1e0bffb5471ced101786e9d8c0f
AndrewKust <deradus@ukr.net>
2022-12-30 10:56:50 +0200

    initial commit

---
+++ Kala/Kala/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kala
+//
+//  Created by Andrew Kuzmich on 30.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kala/Kala/KalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalaApp.swift
+//  Kala
+//
+//  Created by Andrew Kuzmich on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct KalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
