49cf97a4fd6b86597573b8b6a3b415606cf34536
UKS <skulptorr@gmail.com>
2023-02-04 00:37:19 +0200

    code refactoring

--- Kala/Kala/Config.swift
+++ Kala/Kala/Config.swift
@@ -16,3 +16,49 @@ class Config: ObservableObject {
     
     @AppStorage("Save_isGoing") var isGoing: Bool = false
 }
+
+//////////////////////////
+///HELPERS
+//////////////////////////
+
+enum ActionTimerStopped: Int, RawRepresentable, CaseIterable {
+    case SaveAndClose = 0
+    case NewSessionFromScratch = 1
+    case AskAction = 9
+}
+
+extension ActionTimerStopped {
+    func asStr() -> LocalizedStringKey {
+        switch self {
+        case .AskAction:
+            return "Запрашивать что выполнить"
+        case .NewSessionFromScratch:
+            return "Новая сессия стартует с 00.00.0"
+        case .SaveAndClose:
+            return "Закрыть с сохранением"
+        }
+    }
+}
+
+enum ActionTimerGoing: Int, RawRepresentable, CaseIterable {
+    case SaveAndClose = 0
+    case NewSessionFromScratch = 1
+    case AskAction = 2
+    case TimeGoingOnKalaClose = 3
+}
+
+extension ActionTimerGoing {
+    func asStr() -> LocalizedStringKey {
+        switch self {
+        case .AskAction:
+            return "Запрашивать что выполнить"
+        case .NewSessionFromScratch:
+            return "Новая сессия стартует с 00.00.0"
+        case .SaveAndClose:
+            return "Закрыть с сохранением"
+        case .TimeGoingOnKalaClose:
+            return "Продолжить измерять время пока Kala закрыта"
+        }
+    }
+}
+
