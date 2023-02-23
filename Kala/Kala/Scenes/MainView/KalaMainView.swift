7c4f2541a9ce75cdf35c2667cc65ab9d7afad389
UKS <skulptorr@gmail.com>
2023-02-23 22:33:38 +0200

    askAlert1 fixed p2

--- Kala/Kala/Scenes/MainView/KalaMainView.swift
+++ Kala/Kala/Scenes/MainView/KalaMainView.swift
@@ -130,37 +130,8 @@ extension NSWindow {
                 MainViewModel.shared.config.timePassedInterval = CFTimeInterval(0)
                 NSApplication.shared.terminate(self)
             case .AskAction:
-                askAlert1()
+                AppDelegate.instance.showCustomAlert()
             }
         }
-    }
-
-    
-    func askAlert1() { // Rename me, this is peace of shit
-        AppDelegate.instance.showCustomAlert()
-        
-        
-//        let alert = NSAlert()
-//        alert.messageText = "Предупреждение"
-//        alert.informativeText = "Вы уверены, что хотите закрыть приложение?"
-//        alert.addButton(withTitle: "Закрыть с сохранением")
-//        alert.addButton(withTitle: "Нет")
-//        alert.addButton(withTitle: "Не сохранять ")
-//        if MainViewModel.shared.config.isGoing == true {
-//            alert.addButton(withTitle: "Сохранить и продолжить оффлайн?")
-//        }
-//
-//        switch alert.runModal() {
-//        case .alertFirstButtonReturn:
-//            MainViewModel.shared.pause()
-//            NSApplication.shared.terminate(self)
-//        case .alertThirdButtonReturn:
-//            MainViewModel.shared.pause()
-//            MainViewModel.shared.config.timePassedInterval = CFTimeInterval(0)
-//            NSApplication.shared.terminate(self)
-//        default:
-//            alert.window.close()
-//            break;
-//        }
     }
 }
