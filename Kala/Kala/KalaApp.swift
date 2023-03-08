4750baa01a95683114d323d5bad13c35bafd6840
UKS <skulptorrr@gmail.com>
2023-03-08 02:59:23 +0200

    codestyle fixes

--- Kala/Kala/KalaApp.swift
+++ Kala/Kala/KalaApp.swift
@@ -8,23 +8,8 @@ struct KalaApp: App {
         WindowGroup {
             KalaMainView()
         }
-        .windowStyle(HiddenTitleBarWindowStyle())
-        .commands {
-            
-            CommandGroup(replacing: .newItem, addition: { })
-            CommandGroup(replacing: .undoRedo, addition: { })
-            CommandGroup(replacing: .toolbar, addition: { })
-            CommandGroup(replacing: .windowList, addition: { })
-            CommandGroup(replacing: .pasteboard, addition: { })
-            CommandGroup(replacing: .windowSize, addition: { })
-            CommandGroup(replacing: .appInfo, addition: { })
-            CommandGroup(replacing: .help, addition: { })
-            CommandGroup(replacing: .appTermination, addition: { })
-            CommandGroup(replacing: .appVisibility, addition: { })
-//            CommandGroup(replacing: .sidebar, addition: { })
-//            CommandGroup(replacing: .printItem, addition: { })
-            
-        }
+        .windowStyle(.hiddenTitleBar)
+        .removeUselessMenus()
         .replaceAbout { appDelegate.showAboutWnd() }
         
         Settings {
@@ -37,12 +22,28 @@ struct KalaApp: App {
 ///HELPERS
 ////////////////////
 
-extension Scene {
+fileprivate extension Scene {
     func replaceAbout(act: @escaping () -> () ) -> some Scene {
         self.commands {
             CommandGroup(replacing: CommandGroupPlacement.appInfo) {
                 Button("About \(Bundle.main.appName)") { act() }
             }
+        }
+    }
+    
+    @SceneBuilder
+    func removeUselessMenus() -> some Scene {
+        self.commands {
+            CommandGroup(replacing: .newItem, addition: { })
+            CommandGroup(replacing: .undoRedo, addition: { })
+            CommandGroup(replacing: .toolbar, addition: { })
+            CommandGroup(replacing: .windowList, addition: { })
+            CommandGroup(replacing: .pasteboard, addition: { })
+            CommandGroup(replacing: .windowSize, addition: { })
+            CommandGroup(replacing: .appInfo, addition: { })
+            CommandGroup(replacing: .help, addition: { })
+            CommandGroup(replacing: .appTermination, addition: { })
+            CommandGroup(replacing: .appVisibility, addition: { })
         }
     }
 }
